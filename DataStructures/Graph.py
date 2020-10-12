674a990459b5fcec8138c6797469d501e7cc5aed
Sergey <sergey_kirakosyan@edu.aua.am>
2020-10-12 11:35:01 +0400

    added breadth first search graph traversal

--- DataStructures/Graph.py
+++ DataStructures/Graph.py
@@ -1,3 +1,26 @@
+class Queue:
+
+    def __init__(self, items=None) -> None:
+        if items is None:
+            items = []
+        self.queue: list = items
+
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.queue)
+
+    def __repr__(self):
+        return self.queue
+
+
 class Vertex:
 
     def __init__(self, name):
@@ -31,6 +54,24 @@ class Graph:
         for key in sorted(list(self.vertices.keys())):
             print(key, sorted(list(self.vertices[key].neighbours)))
 
+    def __getitem__(self, vertex_name):
+        return self.vertices[vertex_name]
+
+
+def breadth_first_search(graph: Graph, root):
+    visited_vertices = list()
+    graph_queue = Queue([root])
+    visited_vertices.append(root)
+    while len(graph_queue) > 0:
+        node = graph_queue.dequeue()
+        adj_nodes = graph[node]
+        remaining_elements = set(adj_nodes).difference(set(visited_vertices))
+        if len(remaining_elements) > 0:
+            for elem in sorted(remaining_elements):
+                visited_vertices.append(elem)
+                graph_queue.enqueue(elem)
+    return visited_vertices
+
 
 g = Graph()
 a = Vertex("A")
@@ -39,3 +80,4 @@ g.add_vertex(a)
 g.add_vertex(b)
 g.add_edge(a.name, b.name)
 g.print_graph()
+print(breadth_first_search(g, 'A'))
