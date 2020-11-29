6bfc8f78333c5daed1a4b233e485f968a3989b79
Sergey <sergey_kirakosyan@edu.aua.am>
2020-11-29 13:50:45 +0300

    done testing

--- HW/DataStructures/HashTable.py
+++ HW/DataStructures/HashTable.py
@@ -1,13 +1,21 @@
 class HashItem:
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
+
+    def __repr__(self):
+        if isinstance(self.value, bool):
+            return f"{self.key}: bool {self.value}"
+        elif isinstance(self.value, str):
+            return f"{self.key}: str {self.value}"
+        return f"{self.key}: int {self.value}"
 
 
 class HashTable:
 
     def __init__(self):
-        self.size = 256
+        self.size = 16
         self.slots = [None for i in range(self.size)]
         self.count = 0
 
@@ -20,6 +28,7 @@ class HashTable:
         return hash_value % self.size
 
     def put(self, key, value):
+        print(f"Putting {value} at key: {key}...")
         item = HashItem(key, value)
         hash_value = self._hash(key)
         while self.slots[hash_value] is not None:
@@ -38,11 +47,58 @@ class HashTable:
             hash_value = (hash_value + 1) % self.size
         return None
 
+    def remove(self, key):
+        print(f"Removing at key: {key}...")
+        index = self._hash(key)
+        while self.slots[index] is not None:
+            if self.slots[index].key == key:
+                break
+            index = self._hash(index + 1)
+        if self.slots[index] is None:
+            return
+        else:
+            del self.slots[index]
+
     def __setitem__(self, key, value):
         self.put(key, value)
 
     def __getitem__(self, key):
         return self.get(key)
 
+    def __repr__(self):
+        return str(self.slots)
 
-ht = HashTable()
+
+def main_testing():
+    """Testing all functions of HashTable class"""
+    print("Testing HashTable class methods...\n")
+
+    hashtable = HashTable()
+
+    # testing put function
+    hashtable.put("3", 3)
+    hashtable.put("2", 2)
+    hashtable.put("1", "1")
+    hashtable.put("1300", True)
+    print()
+    print(hashtable)
+    if hashtable.slots[1].value == "1" and hashtable.slots[2].value == 2:
+        print("Testing put method: PASS\n")
+    else:
+        print("Testing put method: FAIL\n")
+
+    # testing remove function
+    hashtable.remove("3")
+    hashtable.remove("1")
+    print()
+    print(hashtable)
+    if hashtable["1"] is None and hashtable["3"] is None:
+        print("Testing put method: PASS\n")
+    else:
+        print("Testing put method: FAIL\n")
+
+    print("Methods __getitem__, __setitem__, and get are tested since methods put and remove use them...")
+
+
+if __name__ == '__main__':
+    main_testing()
